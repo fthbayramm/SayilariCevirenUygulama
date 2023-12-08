598c73196b68047d24368079cce7f0809043698f
FATİH BAYRAM <bayramfatih235@gmail.com>
2023-12-08 17:12:23 +0300

    SayilariCevirenFormUygulamasi
    Sayilari Romen Rakamına Çeviren C# Form Uygulaması

---
+++ sayilaricevir/sayilaricevir/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace sayilaricevir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb = new System.Windows.Forms.TextBox();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(262, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayıları Çeviren Uygulama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(34, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Herhangi bir sayı giriniz:";
+            // 
+            // tb
+            // 
+            this.tb.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb.Location = new System.Drawing.Point(38, 158);
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(740, 28);
+            this.tb.TabIndex = 2;
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.LightSalmon;
+            this.b2.Location = new System.Drawing.Point(298, 204);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(172, 43);
+            this.b2.TabIndex = 4;
+            this.b2.Text = "Temizle";
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.LightSalmon;
+            this.b3.Location = new System.Drawing.Point(606, 204);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(172, 43);
+            this.b3.TabIndex = 5;
+            this.b3.Text = "Çıkış";
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(34, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Çevirilen Sonuç:";
+            // 
+            // mb
+            // 
+            this.mb.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mb.Location = new System.Drawing.Point(38, 313);
+            this.mb.Multiline = true;
+            this.mb.Name = "mb";
+            this.mb.Size = new System.Drawing.Size(740, 218);
+            this.mb.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSalmon;
+            this.button1.Location = new System.Drawing.Point(38, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 43);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Çevir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1190, 570);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.tb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox mb;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ sayilaricevir/sayilaricevir/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayilaricevir
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string rs = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string number = tb.Text;
+            int n = int.Parse(number);
+            while (n != 0)
+            {
+                int rem = n % 10;
+                find(rem);
+                n = n / 10;
+            }
+            mb.Text = rs;
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            tb.Text = "";
+            mb.Text = "";
+            rs = "";
+
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        void find(int num)
+        {
+            if (num == 0)
+            {
+                rs = "Zero" + rs;
+            }
+            else if (num == 1)
+            {
+                rs = "|" + rs;
+            }
+            else if (num == 2)
+            {
+                rs = "||" + rs;
+            }
+            else if (num == 3)
+            {
+                rs = "|||" + rs;
+            }
+            else if (num == 4)
+            {
+                rs = "Four" + rs;
+            }
+            else if (num == 5)
+            {
+                rs = "v" + rs;
+            }
+            else if (num == 6)
+            {
+                rs = "Six" + rs;
+            }
+            else if (num == 7)
+            {
+                rs = "Seven" + rs;
+            }
+            else if (num == 8)
+            {
+                rs = "Eight" + rs;
+            }
+            else if (num == 9)
+            {
+                rs = "Nine" + rs;
+            }
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
